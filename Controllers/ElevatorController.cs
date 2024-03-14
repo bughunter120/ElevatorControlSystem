43e80ebeded8fbc150c48454717d92e59ba2e1ed
bughunter120 <elazarusb02@gmail.com>
2024-03-14 09:56:59 -0500

    assessment

---
+++ Controllers/ElevatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ElevatorControlSystem.Models;
+using System.Threading.Tasks;
+
+namespace ElevatorControlSystem.Controllers
+{
+    public class ElevatorController : Controller
+    {
+        public static Elevator Elevator = new Elevator();
+
+        public IActionResult Index()
+        {
+            return View(Elevator);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CallElevator(int floor)
+        {
+            Elevator.CurrentFloor = floor;
+            Elevator.DoorsOpen = true; 
+            await Task.Delay(3000); // Simulate delay for the elevator moving
+            return Json(new { success = true, currentFloor = Elevator.CurrentFloor, doorsOpen = Elevator.DoorsOpen });
+        }
+
+        [HttpPost]
+        public IActionResult MoveElevator(int targetFloor)
+        {
+
+            Elevator.CurrentFloor = targetFloor;
+            Elevator.DoorsOpen = false;
+            return Json(new { success = true, currentFloor = Elevator.CurrentFloor, doorsOpen = Elevator.DoorsOpen });
+        }
+
+        [HttpPost]
+        public IActionResult OpenCloseDoors(bool open)
+        {
+            Elevator.DoorsOpen = open;
+            return Json(new { success = true, doorsOpen = Elevator.DoorsOpen });
+        }
+
+    }
+}
---
+++ Models/Elevator.cs
@@ -0,0 +1,11 @@
+﻿namespace ElevatorControlSystem.Models
+{
+    public enum Direction { Up, Down, Stationary }
+
+    public class Elevator
+    {
+        public int CurrentFloor { get; set; } = 1;
+        public bool DoorsOpen { get; set; } = false;
+        public Direction Direction { get; set; } = Direction.Stationary;
+    }
+}
